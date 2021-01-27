f0785171af2f9c833fabf459217e3790609bd01e
YanaMan <manaevaiana@gmail.com>
2021-01-27 19:28:43 +0300

    Initial commit

---
+++ src/One2.java
@@ -0,0 +1,8 @@
+public class One2 {
+    public static void main(String[] args) {
+        int I = 5;
+        int IV = 1;
+        int result = I-IV;
+        System.out.println("Результат:"+result);
+    }
+}
